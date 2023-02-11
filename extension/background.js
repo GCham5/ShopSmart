4d12728af2ba4f4c024796e3e13af133eafbfc99
Mustafa Ali <alimustafa0987@gmail.com>
2023-02-11 14:35:33 -0500

    add abckground script

---
+++ extension/background.js
@@ -0,0 +1,30 @@
+const tabChange = (tabId, event) => {
+    console.log(event)
+    chrome.scripting.insertCSS({
+        target: {tabId: tabId},
+        files: ["./css/foreground.css"]
+    })
+    .then(() => {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ["./foreground.js"]
+        })
+    }).catch(err => console.log(err));             
+}
+
+//gone use this one for changing the tab b/w existing tabs (includes when u a close a tab and u auto go to another tab)
+chrome.tabs.onActivated.addListener(activeTabInfo => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+        if (/^http/.test(tabs[0].url)) {
+            tabChange(activeTabInfo.tabId, "onActivated");
+        }
+   })
+})
+
+
+//gonna use this one for when u create a new tab, or when you change the url of the cur tab ur on
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status == 'complete' && /^http/.test(tab.url)) {
+        tabChange(tabId, "onUpdated");
+    }
+});
