9567b4c1f47351b1d8990fd770aeb82f37e3fab5
Mustafa Ali <alimustafa0987@gmail.com>
2023-02-12 02:42:18 -0500

    add actions

--- extension/foreground.js
+++ extension/foreground.js
@@ -2,6 +2,8 @@ const mousepos = [0,0];
 const events = [];
 let scrolly = 0;
 let mouseMonitorInterval;
+let sessionTimer = 0;
+let sessionTimerInterval;
 let sessionDataforPage = {}
 const gridSize = 50
 let heatmapactive = false;
@@ -13,6 +15,24 @@ document.addEventListener('mousemove',(e)=>{
 document.addEventListener('scroll',(e)=>{
     scrolly = window.scrollY;
 });
+
+document.body.addEventListener('click', (e) => {
+    const item = e.target
+    const hasChildren = item.children.length > 0;
+
+    let inner = "";
+    if (hasChildren) {
+        inner = item.innerHTML
+        item.innerHTML = ''
+    }
+    const outer = item.outerHTML;
+    if (hasChildren) {
+        item.innerHTML = inner;
+    }
+    
+    chrome.runtime.sendMessage({ message: "tobg_action", payload: {target: outer, time: sessionTimer, type:'click'}})
+})
+
 
 const drawHeatMapSquare = (ctx, x, y) => {
     const col = Math.floor(x/gridSize)
@@ -33,7 +53,6 @@ generateHeatMap = () => {
     sessionDataforPage.mousePos.forEach(mousePos => {
         drawHeatMapSquare(ctx,mousePos[0], mousePos[1])
     })
-    // ctx.fillRect(0,0,500,500)
     document.getElementsByTagName('body')[0].appendChild(canvas)
 
 }
@@ -43,9 +62,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         sendResponse(true)
     } else if (request.message === 'tofg_start_recording') {
         clearInterval(mouseMonitorInterval);
+        clearInterval(sessionTimerInterval);
         mouseMonitorInterval = setInterval(() => {
             chrome.runtime.sendMessage({ message: "tobg_mousemovedata", payload: {mousePos: [mousepos[0],mousepos[1]+scrolly]}})
         }, 100);
+        sessionTimerInterval = setInterval(() => {
+            sessionTimer += 1;
+        }, 1000)
         setTimeout(()=>{
             clearInterval(mouseMonitorInterval)
         }, 120000)
